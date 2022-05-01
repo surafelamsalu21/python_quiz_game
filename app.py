1f0c1e7f54b79de9e0b95cd587cc5d1048561d80
Surafel <surafelamsalu2013@gmail.com>
2022-05-01 11:23:07 +0300

    Added some functionalty to my app

--- app.py
+++ app.py
@@ -7,8 +7,35 @@ if playing != "yes":
     quit()  # quit() is a python syntax to close the application
 print("Okay! lets play")
 
-answer = input("What dose CPU stands for \n")
+score = 0
+
+
+answer = input("What dose CPU stands for?\n")
 if answer.lower() == "central processing unit":
-    print("Correct!")
+    print("Correct!\n")
+    score += 1
 else:
-    print("Incorrect!")
+    print("Incorrect!\n")
+
+answer = input("What dose GPU stands for?\n")
+if answer.lower() == "graphics processing unit":
+    print("Correct!\n")
+    score += 1
+else:
+    print("Incorrect!\n")
+
+answer = input("What dose RAM stands for?\n")
+if answer.lower() == "random access memory":
+    print("Correct!\n")
+    score += 1
+else:
+    print("Incorrect!\n")
+
+answer = input("What dose PSU stands for \n")
+if answer.lower() == "power supply":
+    print("Correct!\n")
+    score += 1
+else:
+    print("Incorrect!\n")
+
+print(f"The total result is: {score} out of 4")
