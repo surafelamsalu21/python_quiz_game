0a6081eed43361d6fd793bc05db1cef73660b707
Surafel <surafelamsalu2013@gmail.com>
2022-05-01 11:08:09 +0300

    Added some functionalty to my app

--- app.py
+++ app.py
@@ -1,1 +1,14 @@
-print("It is working")
+print("Welcome to my computer quiz!")
+
+playing = input("Do you want to play? \n")
+
+
+if playing != "yes":
+    quit()  # quit() is a python syntax to close the application
+print("Okay! lets play")
+
+answer = input("What dose CPU stands for \n")
+if answer.lower() == "central processing unit":
+    print("Correct!")
+else:
+    print("Incorrect!")
